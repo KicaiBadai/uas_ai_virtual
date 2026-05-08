dcf5087fe42984df04daab9440634e8fdb8d8ef8
KicaiBadai <adityamaula80@gmail.com>
2026-05-08 11:28:19 +0700

    member

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -61,6 +61,7 @@ class AIController extends Controller
 
             $faq = [
 
+                'member pkw' => 'tusi, turusiee, to see, tur to see, 2 sea, two sea, two see',
                 'maestro' => 'maestro adalah tokoh utama dalam kemederkaan indonesia. dia telah memimpin pertempuran legenda di wallet 1980',
                 'pkw' => 'pkw adalah partai komunis wallet yang mempunyai tujuan untuk menggeserkan sang maestro',
                 'jam buka' => 'Toko buka setiap hari jam 08:00 sampai 22:00.',
